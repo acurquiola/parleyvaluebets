9705924d7fe50d5fc689a1652f4b48607c6a8107
Ana Urquiola <anitaurquiola28@gmail.com>
2016-09-27 16:58:36 -0400

    Orden de la tabla de historial de acceso

--- app/Listeners/SaveLogin.php
+++ app/Listeners/SaveLogin.php
@@ -29,14 +29,14 @@ class SaveLogin
     public function handle(Login $event)
     { 
         session(['username' => $event->user->username]);
-        //$location  = GeoIP::getLocation();
+        $location  = GeoIP::getLocation();
         $historial = new \App\Models\AccesoUsuario();
         $historial->fecha_entrada = \Carbon\Carbon::now()->toDateString();
         $historial->hora_entrada  = \Carbon\Carbon::now()->toTimeString();
         $historial->user_id       = $event->user->id;
         $historial->ip            = Request::ip();
-        $historial->pais          = 'Venezuela';
-        //$historial->pais          = $location['country'];
+        //$historial->pais          = 'Venezuela';
+        $historial->pais          = $location['country'];
         if($historial->save()){
             session(['acceso' => $historial->id]);
         };
