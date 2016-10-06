f6e2628bf2be781662c4dc9ae735d782b68e933c
Ana Urquiola <anitaurquiola28@gmail.com>
2016-10-05 22:44:39 -0400

    NFL + competiciones

--- resources/views/futbolAmericano/index.blade.php
+++ resources/views/futbolAmericano/index.blade.php
@@ -26,37 +26,101 @@
 		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Apuestas de Ganador Final</strong></h6>
 
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Super Bowl Outright']) }}">Ganador de Super Bowl</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Super Bowl Outright']) }}">Ganador de Super Bowl</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Division Winner']) }}">Ganador de División</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Division Winner']) }}">Ganador de División</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Conference Winner']) }}">Ganador de Conferencia</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Conference Winner']) }}">Ganador de Conferencia</a></li>
 	                                            </ul>
 
 		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Apuestas por Encuentros</strong></h6>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Money Line']) }}">Ganador del Partido</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Money Line']) }}">Ganador del Partido</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total Points']) }}">Total de Puntos</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Spread']) }}">Ganador con Hándicap</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- To Win + Total Points Double']) }}">Doble al ganador + Total de Puntos</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Total Points']) }}">Total de Puntos</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Winning Margins']) }}">Márgen de Victoria</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Double Result']) }}">Doble Resultado (Descanso / Final)</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Will There Be Overtime?']) }}">¿Habrá Prórroga?</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Match Spread + Total Points Double']) }}">Doble al Hándicap + Total de Puntos</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => 'Spread']) }}">Ganador con Hándicap</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- First Touchdown Scorer']) }}">Primer Jugador en anotar TD</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Anytime Touchdown Score']) }}">Jugador que anota un TD en cualquier momento</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Last Touchdown Scorer']) }}">Último jugador en anotar TD</a></li>
 	                                            </ul>
 
-		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Apuestas en Primera Mitad</strong></h6>
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Apuestas por Mitades</strong></h6>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '1st Half Money Line']) }}">Ganador - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Half Money Line']) }}">Ganador - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Half Winning Margins']) }}">Margen de Victoria - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 2nd Half - 1st Team Touchdown']) }}">Primer Equipo en anotar TD - Segunda mitad</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '1st Half Total Points']) }}">Total de Puntos - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Half Total Points']) }}">Total de Puntos - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 2nd Half Winning Margins']) }}">Margen de Victoria - Segunda Mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total 2nd Half Touchdowns']) }}">Total de TD - Segunda Mitad</a></li>
 	                                            </ul>
 	                                            <ul class="nav navbar-nav col-sm-4" >
-	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '1st Half Money Line']) }}">Ganador con Hándicap- Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Half Money Line']) }}">Ganador con Hándicap - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total 1st Half Touchdowns']) }}">Total deTD - Primera mitad</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Half Result/2nd Half Result']) }}">Resultado 1er Tiempo / 2do Tiempo</a></li>
 	                                            </ul>
-	                                           
+
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Apuestas por cuartos</strong></h6>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Home Team Quarters Won']) }}">Cuartos ganados por el equipo local</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Totales</strong></h6>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Alternative Total Points']) }}">Total de Puntos Alternativo</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total Home Touchdowns']) }}">Total de TD por el equipo local</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total Match Points Odd/Even']) }}">Total Par/Impar</a></li>
+	                                            </ul>
+
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Hándicaps</strong></h6>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Alternative Handicaps']) }}">Hándicap Alternativo</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Anotación</strong></h6>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Team Touchdown']) }}">Primer equipo en conseguir TD</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                            </ul>
+
+		    									<h6 style="margin-left: 15px" class="col-sm-12"><strong>Estadísticas</strong></h6>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Will There Be A Safety?']) }}">¿Habrá Safety?</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- 1st Team To Turnover']) }}">Primer equipo en conseguir Turnover</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Total Match Turnovers']) }}">Total de Turnovers del partido</a></li>
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Team With Most Punts']) }}">Equipo con más punts</a></li>
+	                                            </ul>
+	                                            <ul class="nav navbar-nav col-sm-4" >
+	                                                <li class=" col-sm-12" ><a href="{{ action('FutbolAmericanoController@getCompetencias', ['type' => 'NFL', 'name' => '- Team With Most Turnovers']) }}">Equipo con más Turnovers</a></li>
+	                                            </ul>
 		                                    </ul>
     									</div>
 
