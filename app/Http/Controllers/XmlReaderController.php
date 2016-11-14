8a2d48d6e1d46ab9bec4e7a1af27bd25be8ffd3c
Ana Urquiola <anitaurquiola28@gmail.com>
2016-11-14 10:59:16 -0430

    Errores en carga de datos

--- app/Http/Controllers/XmlReaderController.php
+++ app/Http/Controllers/XmlReaderController.php
@@ -71,57 +71,75 @@ class XmlReaderController extends Controller
                     ];
 
     	foreach ($rutas as $index => $ruta) {
-    		try {
-                $xml     = XmlParser::load($ruta);
-                $content = $xml->getContent();
+            $xml     = XmlParser::load($ruta);
+            $content = $xml->getContent();
 
-                if(isset($content->response->williamhill->class)){
+            if(isset($content->response->williamhill->class)){
 
-                    foreach ($content->response->williamhill->class as $class) {
-                        $clase = Clase::where('claseID', $class['id'])->orderBy('created_at', 'DESC')->first();
-                        if ($clase == NULL){
-                            $classNew             = new  CLase();
-                            $classNew->claseID    = $class['id'];
-                            $classNew->name       = $class['name'];
-                            $classNew->maxRepDate = $class['maxRepDate'];
-                            $classNew->maxRepTime = $class['maxRepTime'];
-                            $classNew->save();
+                foreach ($content->response->williamhill->class as $class) {
+                    $clase = Clase::where('claseID', $class['id'])->orderBy('created_at', 'DESC')->first();
+                    if ($clase == NULL){
+                        $classNew             = new  CLase();
+                        $classNew->claseID    = $class['id'];
+                        $classNew->name       = $class['name'];
+                        $classNew->maxRepDate = $class['maxRepDate'];
+                        $classNew->maxRepTime = $class['maxRepTime'];
+                        $classNew->save();
+                    }else{
+                        $classNew = $clase;
+                    }
+                    foreach ($content->response->williamhill->class->type as $type) {
+                        $types = Type::where('typeID', $type['id'])->orderBy('created_at', 'DESC')->first();
+                        if ($types == NULL){
+                            $typeNew                 = new  Type();
+                            $typeNew->typeID         = $type['id'];
+                            $typeNew->name           = $type['name'];
+                            $typeNew->lastUpdateDate = $type['lastUpdateDate'];
+                            $typeNew->lastUpdateTime = $type['lastUpdateTime'];
+                            $typeNew->clase_id       = $classNew->id;
+                            $typeNew->save();
                         }else{
-                            $classNew = $clase;
+                            $typeNew = $types;
                         }
-                        foreach ($content->response->williamhill->class->type as $type) {
-                            $types = Type::where('typeID', $type['id'])->orderBy('created_at', 'DESC')->first();
-                            if ($types == NULL){
-                                $typeNew                 = new  Type();
-                                $typeNew->typeID         = $type['id'];
-                                $typeNew->name           = $type['name'];
-                                $typeNew->lastUpdateDate = $type['lastUpdateDate'];
-                                $typeNew->lastUpdateTime = $type['lastUpdateTime'];
-                                $typeNew->clase_id       = $classNew->id;
-                                $typeNew->save();
+                        foreach ($type->market as $market) {
+                            //Se busca el Market registrado con el ID leído.
+                            $markets = Market::with('participants')
+                            ->where('marketID', $market['id'])
+                            ->first();
+
+                            if ($markets == NULL){
+                                //Si no existe se crea.
+                                $marketNew                 = new  Market();
+                                $marketNew->marketID       = $market['id'];
+                                $marketNew->name           = $market['name'];
+                                $marketNew->betTillDate    = $market['betTillDate'];
+                                $marketNew->betTillTime    = $market['betTillTime'];
+                                $marketNew->lastUpdateDate = $market['lastUpdateDate'];
+                                $marketNew->lastUpdateTime = $market['lastUpdateTime'];
+                                $marketNew->type_id        = $typeNew->id;
+                                $marketNew->save();
+
+                                //Para cada uno de los markets se registran los partipants correspondientes.
+                                foreach ($market->participant as $participant) {
+                                    $participantNew                 = new  Participant();
+                                    $participantNew->participantID  = $participant['id'];
+                                    $participantNew->name           = $participant['name'];
+                                    $participantNew->oddsDecimal    = $participant['oddsDecimal'];
+                                    $participantNew->lastUpdateDate = $participant['lastUpdateDate'];
+                                    $participantNew->lastUpdateTime = $participant['lastUpdateTime'];
+                                    $participantNew->handicap       = $participant['handicap'];
+                                    $participantNew->market_id      = $marketNew->id;
+                                    $participantNew->save();
+                                }
                             }else{
-                                $typeNew = $types;
-                            }
-                            foreach ($type->market as $market) {
-                                //Se busca el Market registrado con el ID leído.
-                                $markets = Market::with('participants')
-                                ->where('marketID', $market['id'])
-                                ->first();
+                                foreach ($market->participant as $participant) {
 
-                                if ($markets == NULL){
-                                    //Si no existe se crea.
-                                    $marketNew                 = new  Market();
-                                    $marketNew->marketID       = $market['id'];
-                                    $marketNew->name           = $market['name'];
-                                    $marketNew->betTillDate    = $market['betTillDate'];
-                                    $marketNew->betTillTime    = $market['betTillTime'];
-                                    $marketNew->lastUpdateDate = $market['lastUpdateDate'];
-                                    $marketNew->lastUpdateTime = $market['lastUpdateTime'];
-                                    $marketNew->type_id        = $typeNew->id;
-                                    $marketNew->save();
+                                    //Se busca el Participant registrado con el ID leído.
+                                    $participants = Participant::where('participantID', $participant['id'])
+                                    ->first();
 
-                                    //Para cada uno de los markets se registran los partipants correspondientes.
-                                    foreach ($market->participant as $participant) {
+                                    if ($participants == NULL){
+                                        //Si no existe se crea un nuevo participant
                                         $participantNew                 = new  Participant();
                                         $participantNew->participantID  = $participant['id'];
                                         $participantNew->name           = $participant['name'];
@@ -129,57 +147,18 @@ class XmlReaderController extends Controller
                                         $participantNew->lastUpdateDate = $participant['lastUpdateDate'];
                                         $participantNew->lastUpdateTime = $participant['lastUpdateTime'];
                                         $participantNew->handicap       = $participant['handicap'];
-                                        $participantNew->market_id      = $marketNew->id;
+                                        $participantNew->market_id      = $market->id;
                                         $participantNew->save();
-                                    }
-                                }else{
-                                    foreach ($market->participant as $participant) {
-
-                                        //Se busca el Participant registrado con el ID leído.
-                                        $participants = Participant::where('participantID', $participant['id'])
+                                    }else{
+                                        //Si existe se busca si tiene una incidencia en el histórico
+                                        $participantsHist = HistoricoLogro::where('participantID', $participant['id'])
+                                        ->orderBy('created_at', 'DESC')
                                         ->first();
+                                        if($participantsHist == NULL){
 
-                                        if ($participants == NULL){
-                                            //Si no existe se crea un nuevo participant
-                                            $participantNew                 = new  Participant();
-                                            $participantNew->participantID  = $participant['id'];
-                                            $participantNew->name           = $participant['name'];
-                                            $participantNew->oddsDecimal    = $participant['oddsDecimal'];
-                                            $participantNew->lastUpdateDate = $participant['lastUpdateDate'];
-                                            $participantNew->lastUpdateTime = $participant['lastUpdateTime'];
-                                            $participantNew->handicap       = $participant['handicap'];
-                                            $participantNew->market_id      = $market->id;
-                                            $participantNew->save();
-                                        }else{
-                                            //Si existe se busca si tiene una incidencia en el histórico
-                                            $participantsHist = HistoricoLogro::where('participantID', $participant['id'])
-                                            ->orderBy('created_at', 'DESC')
-                                            ->first();
-                                            if($participantsHist == NULL){
-
-                                                //Si no tiene incidencia y la fecha y hora del registro son menores a la del ultimo registro guardado se crea una nueva incidencia
-                                                if (($participants->handicap != $participant['handicap']) || ($participants->lastUpdateDate < $participant['lastUpdateDate']) || (($participants->lastUpdateDate = $participant['lastUpdateDate'])  && $participants->lastUpdateTime < $participant['lastUpdateTime'] )){
-                                                    if ($participants->oddsDecimal != $participant['oddsDecimal']){
-                                                        $marketNew = $markets;
-                                                        $participantHist                 = new  HistoricoLogro();
-                                                        $participantHist->participantID  = $participant['id'];
-                                                        $participantHist->name           = $participant['name'];
-                                                        $participantHist->oddsDecimal    = $participant['oddsDecimal'];
-                                                        $participantHist->lastUpdateDate = $participant['lastUpdateDate'];
-                                                        $participantHist->lastUpdateTime = $participant['lastUpdateTime'];
-                                                        $participantHist->handicap       = $participant['handicap'];
-                                                        $participantHist->participant_id = $participants->id;
-                                                        $participantHist->save();
-                                                        $participants->update(['isChange'=>true]);
-                                                    }else{
-                                                        $participants->update(['handicap'=>$participant['handicap'],'lastUpdateDate'=>$participant['lastUpdateDate'],'lastUpdateTime'=>$participant['lastUpdateTime']]);
-                                                    }
-                                                }
-                                            //Si tiene histórico se verifican la fecha y hora de creación si es menor la almacenada se registran
-                                            }elseif(($participants->handicap != $participant['handicap']) || ($participantsHist->lastUpdateDate < $participant['lastUpdateDate']) || (($participantsHist->lastUpdateDate = $participant['lastUpdateDate'])  && $participantsHist->lastUpdateTime < $participant['lastUpdateTime'] )){
-                                                //Si existe el participant buscado y no ha sido cambiado se almacena directamente en el histórico
+                                            //Si no tiene incidencia y la fecha y hora del registro son menores a la del ultimo registro guardado se crea una nueva incidencia
+                                            if (($participants->handicap != $participant['handicap']) || ($participants->lastUpdateDate < $participant['lastUpdateDate']) || (($participants->lastUpdateDate = $participant['lastUpdateDate'])  && $participants->lastUpdateTime < $participant['lastUpdateTime'] )){
                                                 if ($participants->oddsDecimal != $participant['oddsDecimal']){
-
                                                     $marketNew = $markets;
                                                     $participantHist                 = new  HistoricoLogro();
                                                     $participantHist->participantID  = $participant['id'];
@@ -190,9 +169,28 @@ class XmlReaderController extends Controller
                                                     $participantHist->handicap       = $participant['handicap'];
                                                     $participantHist->participant_id = $participants->id;
                                                     $participantHist->save();
+                                                    $participants->update(['isChange'=>true]);
                                                 }else{
                                                     $participants->update(['handicap'=>$participant['handicap'],'lastUpdateDate'=>$participant['lastUpdateDate'],'lastUpdateTime'=>$participant['lastUpdateTime']]);
                                                 }
+                                            }
+                                        //Si tiene histórico se verifican la fecha y hora de creación si es menor la almacenada se registran
+                                        }elseif(($participants->handicap != $participant['handicap']) || ($participantsHist->lastUpdateDate < $participant['lastUpdateDate']) || (($participantsHist->lastUpdateDate = $participant['lastUpdateDate'])  && $participantsHist->lastUpdateTime < $participant['lastUpdateTime'] )){
+                                            //Si existe el participant buscado y no ha sido cambiado se almacena directamente en el histórico
+                                            if ($participants->oddsDecimal != $participant['oddsDecimal']){
+
+                                                $marketNew = $markets;
+                                                $participantHist                 = new  HistoricoLogro();
+                                                $participantHist->participantID  = $participant['id'];
+                                                $participantHist->name           = $participant['name'];
+                                                $participantHist->oddsDecimal    = $participant['oddsDecimal'];
+                                                $participantHist->lastUpdateDate = $participant['lastUpdateDate'];
+                                                $participantHist->lastUpdateTime = $participant['lastUpdateTime'];
+                                                $participantHist->handicap       = $participant['handicap'];
+                                                $participantHist->participant_id = $participants->id;
+                                                $participantHist->save();
+                                            }else{
+                                                $participants->update(['handicap'=>$participant['handicap'],'lastUpdateDate'=>$participant['lastUpdateDate'],'lastUpdateTime'=>$participant['lastUpdateTime']]);
                                             }
                                         }
                                     }
@@ -201,10 +199,8 @@ class XmlReaderController extends Controller
                         }
                     }
                 }
-                Log::info('Logros actualizados exitósamente.');
-            } catch (Exception $e) {
-                Log::info('Error actualizando los logros.');
             }
+            Log::info('Logros actualizados exitósamente.');
     	}
     return redirect('/');
     }
